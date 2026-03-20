3bba73f8b519b6b0842acafa5cfa4881a9b559d4
vasilev.ta <vasilev.ta@dozen.mephi.ru>
2026-03-20 03:39:48 +0300

    try to fix regex(unwork method)

--- RegEx/RegEx.h
+++ RegEx/RegEx.h
@@ -10,20 +10,55 @@
 class RegExClass : public IPars {
 private:
     //мб поделить
-    inline static const std::regex createListRegex{
-            R"(^\s*create\s+([a-zA-Z_.][a-zA-Z0-9_.]*)\s*\((\s*[a-zA-Z_.][a-zA-Z0-9_.]*(\s*,\s*[a-zA-Z_.][a-zA-Z0-9_.]*)*\s*)\)\s*$)"
+    inline static const std::regex createRegex{
+            R"(^\s*create)"
     };
 
-    inline static const std::regex joinListRegex{
-            R"(^\s*create\s+([a-zA-Z_.][a-zA-Z0-9_.]*)\s+as\s+([a-zA-Z_.][a-zA-Z0-9_.]*)\s+join\s+([a-zA-Z_.][a-zA-Z0-9_.]*)\s*$)"
+    inline static const std::regex nameRegex{
+            R"(^\s+([a-zA-Z_.][a-zA-Z0-9_.]*))"
     };
+
+    inline static const std::regex lparenRegex{
+            R"(^\s*\()"
+    };
+
+    inline static const std::regex attrRegex{
+            R"(^\s*([a-zA-Z_.][a-zA-Z0-9_.]*))"
+    };
+
+    inline static const std::regex commaRegex{
+            R"(^\s*,)"
+    };
+
+    inline static const std::regex rparenRegex{
+            R"(^\s*\))"
+    };
+
+    inline static const std::regex asRegex{
+            R"(^\s+as)"
+    };
+
+    inline static const std::regex joinRegex{
+            R"(^\s+join)"
+    };
+
+    inline static const std::regex endRegex{
+            R"(^\s*$)"
+    };
+
+    std::string str = "";
+    size_t      pos =  0;
 
     RelationStore& store;
+
+    bool eat(const std::regex& re);
+
+    std::pair<bool, std::string> eatName();
 
 public:
     explicit RegExClass(RelationStore& relStore);
 
-    std::pair<bool, std::string> pars(std::string_view line);
+    std::pair<bool, std::string> pars(std::string_view line) override;
 };
 
 
